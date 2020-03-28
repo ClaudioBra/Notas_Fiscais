20aea29847930c27711582c368dc77ad4d87f38a
Claudio <claudio.santos57@fatec.sp.gov.br>
2020-03-28 10:19:19 -0300

    atualizando

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,9 +1,17 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
+
+
+const sumItems = code => notas => notas
+    .$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
 
 document
     .querySelector('#myButton')
     .onclick = () =>
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(notas => console.log(notas))
-            .catch(err => console.log(err));
+            .then(sumItems('2143'))
+            .then(console.log)
+            .catch(console.log);
