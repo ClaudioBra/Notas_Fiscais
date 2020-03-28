57c077d959947865410d615992061fc68613534b
Claudio <claudio.santos57@fatec.sp.gov.br>
2020-03-28 19:38:25 -0300

    quase lá

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,17 +1,21 @@
-import { handleStatus, log } from './utils/promise-helpers.js';
+// app/app.js
+import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
+import { notasService as service } from './nota/service.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
 
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
+const action = operations(() => 
+    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+    .then(total => EventEmitter.emit('itensTotalizados', total))
+    .catch(console.log)
+);
 
 document
-    .querySelector('#myButton')
-    .onclick = () =>
-        fetch('http://localhost:3000/notas')
-            .then(handleStatus)
-            .then(sumItems('2143'))
-            .then(console.log)
-            .catch(console.log);
+.querySelector('#myButton')
+.onclick = action;
